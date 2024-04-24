a012795dc474f76d497459f5118550612de28c61
darthvangogh <hulashc@gmail.com>
2024-04-24 19:29:44 +0100

    edits to the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import Login from './Login'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Signup from './Signup'
 
 function App() {
   return (
-    <div>
-      <Login />
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- frontend/src/Signup.js
+++ frontend/src/Signup.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react'
+import { Link } from 'react-router-dom'
 
-function Login() {
+function Signup() {
   return (
     <div className='d-flex justify-content-center align-items-center bg-primary vh-100'>
       <div className='bg-white p-3 rounded w-25'>
-        <h2 className='text-center'>Log in</h2>
+        <h2 className='text-center'>Sign up< /h2>
         <form action="">
+          <div className='mb-3'>
+            <label htmlFor="name"><strong>Name</strong></label>
+            <input type="text" placeholder="Enter Name" className='form-control rounded-0'/>
+          </div>
           <div className='mb-3'>
             <label htmlFor="email"><strong>Email</strong></label>
             <input type="email" placeholder="Enter Email" className='form-control rounded-0'/>
@@ -15,13 +19,13 @@ function Login() {
             <label htmlFor="password"><strong>Password</strong></label>
             <input type="password" placeholder="Enter Password" className='form-control rounded-0'/>
           </div>
-          <button className='btn btn-success w-100 rounded-0'>Log in</button>
+          <button className='btn btn-success w-100 rounded-0'>Sign up</button>
           <p>You agree to our terms and policies</p>
-          <Link to="/signup" className='btn btn-default border w-100 bg-light rounded-0 text-decoration-none'>Create Account</Link>
+          <Link to="/" className='btn btn-default border w-100 bg-light rounded-0 text-decoration-none'>Login</Link>
         </form>
       </div>
     </div>
-  );
+  )
 }
 
-export default Login;
+export default Signup
