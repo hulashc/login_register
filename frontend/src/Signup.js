115c0ac8836acc263eba033fbbc7b951b1ee72da
darthvangogh <hulashc@gmail.com>
2024-04-25 05:43:12 +0100

    edits to the backend and frontend

--- frontend/src/Signup.js
+++ frontend/src/Signup.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
 import Validation from './SignupValidation';
+import axios from 'axios'
 
 
 function Signup() {
@@ -10,17 +11,23 @@ function Signup() {
         email: '',
         password: ''
       })
-  
-      const [errors, setErrors] = useState({})
-  
+
+      const navigate = useNavigate();
+      const [errors, setErrors] = useState({})  
       const handleInput = (event) => {
         setValues(prev => ({...prev, [event.target.name]: event.target.value}))
       }
   
       const handleSubmit = (event) => {
-        event.preventDefault();
-        setErrors(Validation(values));
-      }
+            event.preventDefault();
+            setErrors(Validation(values));
+            if(errors.name === "" && errors.email === "" && errors.password === "") {
+                axios.post('http://localhost:8081/signup', values)
+                .then(res => {
+                    navigate('/');
+                })
+                .catch(err => console.log(err));
+          }
 
   return (
     <div className='d-flex justify-content-center align-items-center bg-primary vh-100'>
